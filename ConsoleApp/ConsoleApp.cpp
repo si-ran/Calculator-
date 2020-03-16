486938d999f0e887db2fe3add90451236a3f9778
xusiran@neotel.cpm.cn <Xsr979708>
2020-03-16 12:17:27 +0800

    init

--- ConsoleApp/ConsoleApp.cpp
+++ ConsoleApp/ConsoleApp.cpp
@@ -1,11 +1,119 @@
-﻿// ConsoleApp.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+﻿// ConsoleApplication1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
 #include <iostream>
+#include <vector>
+#include <queue>
+#include <regex>
+#include<stdlib.h>
+using namespace std;
+
+// 运算符优先级
+int op_pri(string op) {
+    if (op == "+" || op == "-") {
+        return 1;
+    }
+    else if (op == "*" || op == "/") {
+        return 2;
+    }
+    else if (op == "(" || op == ")") {
+        return 0;
+    }
+    else {
+        return -1;
+    }
+}
+
+bool get_math(string str, queue<string>& save_match) {
+    vector<string> save_stack;
+    save_stack.push_back("#");
+    //regex reg_all("^(((?<o>\()[-+]?([0-9]+[-+*/])*)+[0-9]+((?<-o>\))([-+*/][0-9]+)*)+($|[-+*/]))*(?(o)(?!))$");
+    regex reg_all("(([0-9]+)[.]*([0-9]*))(([0-9]+)[.]*([0-9]*)|[+*/)(-])*(([0-9]+)[.]*([0-9]*))");
+    if (regex_match(str, reg_all)) {
+        regex reg("((([0-9]+)([.]*)([0-9]*))|[+*/)(-])");
+        smatch str_match;
+        auto pos = str.cbegin();
+        auto end = str.cend();
+        for (; regex_search(pos, end, str_match, reg); pos = str_match.suffix().first) {
+            regex reg_number("([0-9]+)([.]*)([0-9]*)");
+            if (regex_match(str_match.str(0), reg_number)) {
+                save_match.push(str_match.str(0));
+            }
+            else {
+                while (op_pri(save_stack.back()) >= op_pri(str_match.str(0))) {
+                    save_match.push(save_stack.back());
+                    save_stack.pop_back();
+                }
+                save_stack.push_back(str_match.str(0));
+            }
+        }
+        for (; save_stack.begin() != save_stack.end(); save_stack.pop_back()) {
+            save_match.push(save_stack.back());
+        }
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+double cal_math(queue<string>& save_math) {
+    double res = 0;
+    vector<double> save_stack;
+    for (; !save_math.empty(); save_math.pop()) {
+        string str = save_math.front();
+        cout << str << endl;
+        regex reg_number("([0-9]+)[.]*([0-9]*)");
+        if (regex_match(str, reg_number)) {
+            save_stack.push_back(atof(str.c_str()));
+        }
+        else {
+            if (str == "#")
+                return res;
+            double number1 = save_stack.back();
+            save_stack.pop_back();
+            double number2 = save_stack.back();
+            save_stack.pop_back();
+            if (str == "+") {
+                res = number2 + number1;
+                save_stack.push_back(res);
+            }
+            else if (str == "-") {
+                res = number2 - number1;
+                save_stack.push_back(res);
+            }
+            else if (str == "*") {
+                res = number2 * number1;
+                save_stack.push_back(res);
+            }
+            else if (str == "/") {
+                res = number2 / number1;
+                save_stack.push_back(res);
+            }
+            else {
+                return res;
+            }
+        }
+    }
+    return res;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // 输入值
+    string math = "";
+    // 后缀表达式
+    queue<string> save_math;
+    cout << "请输入计算表达式(不支持括号)" << endl;
+    cin >> math;
+    if (get_math(math, save_math)) {
+        cout << "结果为: " << cal_math(save_math) << endl;
+    }
+    else {
+        cout << "输入计算式格式有误" << endl;
+    }
+
+    return 0;
 }
 
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
