32ea90cf5f2619ed26d8c0cb2145cb12dc79c53e
zhangwanqing0930 <zhangwanqing@ebupe.com>
2020-03-18 15:15:47 +0800

    init

--- ConsoleApp/ConsoleApp.cpp
+++ ConsoleApp/ConsoleApp.cpp
@@ -1,15 +1,56 @@
 ﻿// ConsoleApp.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 
+#include <cstdio>
+#include <cstring>
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+const int maxn = 10000;
 
+int n;
+char expression[maxn + 5];
 
+int read_pos;    //read()读入时最后一个字符的位置+1 
+inline int getNumber(int s) {    //获取s位置起的第一个数字 
+    int number = 0;
+    for (; (expression[s] >= '0' && expression[s] <= '9'); s++)
+        number = number * 10 + expression[s] - '0';
+    read_pos = s;
+    return number;
 }
 
+int solve(int pos, int length) {    
+    int stack[2][maxn + 5], len = 0;    
+    switch (expression[pos]) {
+        case '-': stack[0][++len] = 1, stack[1][len] = getNumber(pos);
+        default: stack[0][++len] = -1, stack[1][len] = getNumber(pos + 1);
+    }
+    pos = read_pos;
+    int temp;
+    while (pos <= length) {
+        switch (expression[pos]) {
+        case '+': stack[0][++len] = 1, stack[1][len] = getNumber(pos + 1); break;
+        case '-': stack[0][++len] = -1, stack[1][len] = getNumber(pos + 1); break;
+        case '*': temp = getNumber(pos + 1), stack[1][len] *= temp; break;
+        case '/': temp = getNumber(pos + 1), stack[1][len] /= temp; break;
+        default:  break;
+        }
+        pos = read_pos;
+    }
+    int result = 0;
+    for (int i = 1; i <= len; i++)    //计算数据
+        result += (stack[0][i] * stack[1][i]);
+    return result;
+}
+
+int main() {
+    std::cout << "请输入算式：";
+    std::cin >> expression;
+    int length = strlen(expression);
+    std::cout << "运算结果为：" << expression << "=" << solve(0, length - 1) << '\n';
+    return 0;
+}
 // 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
 // 调试程序: F5 或调试 >“开始调试”菜单
 
